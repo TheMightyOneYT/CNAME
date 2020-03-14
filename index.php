a8d933021f387aa3f25b8b1d9849424d80ef0634
TheMightyOneYT <52082242+TheMightyOneYT@users.noreply.github.com>
2020-03-14 14:09:20 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!Doctype HTML>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body style = "background-image:url(https://i.cbc.ca/1.3638138.1479852274!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/milky-way-concordia-pakistan.jpg);background-size:cover;background-repeat:no-repeat;">
+<center>
+<div style = "background-color:white;width:70%;">
+<center><h1 style = "color:orange;font-family:verdana;"><big><big><big>DASHBOARD</big></big></big></h1></center>
+<center><h1 style = "color:black;font-family:verdana;">Welcome: User</h1></center>
+<br>
+<hr>
+<center><h2 style = "color:red;font-family:verdana;">Recommended video:</h2></center>
+<center><iframe src = "" frameborder = "0" width = "70%" height = "400"></iframe></center>
+<hr>
+<br>
+<center><h2 style = "color:purple;font-family:verdana;">Our Discord:</h2></center>
+<center><iframe src="https://titanembeds.com/embed/366744314203865089" height="600" width="80%" frameborder="0"></iframe></center>
+<center><iframe src="https://discordapp.com/widget?id=366744314203865089&theme=dark" width="80%" height="600" allowtransparency="true" frameborder="0"></iframe></center>
+</div>
+</center>
+</body>
+</html>
